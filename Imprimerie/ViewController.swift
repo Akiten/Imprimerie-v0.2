727135dd02148d362ce76a75635a43e2b9ca1f1f
Akiten <guillaume.pigasse@gmail.com>
2019-08-03 01:55:58 +0100

    Initial Commit

---
+++ Imprimerie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Imprimerie
+//
+//  Created by MAC on 8/3/19.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
